8bbd018e07e646a4b4f11d94b9ddb8628e1d9481
EsdeathParca <thejokerstark@gmail.com>
2021-08-16 00:01:23 -0300

    Mi primer commit

---
+++ Mi_primer_Script.py
@@ -0,0 +1,36 @@
+print(' Hola Mundo, este es un texto escrito con lenguaje python.')
+print('\n')
+print(' Existió una vez un hombre que sin saber como se llamaba y sin saber a donde')
+print(' pertenecía siempre se preguntó por que él se encontraba donde se encontraba,')
+print(' nada tenía sentido para él, sin embargo decidió caminar hasta que los piés se,')
+print(' volviesen de fuego y las manos se le erocionaran a causa del viento.')
+
+#A partir de aquí inicio con una práctica para conbinar distintos tipos de valores que puede incluir la función print.
+#Agrego valores a funciones definidas para realizar una operación de suma o resta, obtengo un resultado y convino..
+#ese resultado en otra ecuación a partir del valor anterior.
+
+print('\n -----------------------------Sueldo mensual-------------------------------')
+print('Depositado: $68.0000 ars\nC.Al: $10.000 ars')
+a = 68000
+b = 10000
+total = a - b
+print('Saldo:total','ars')
+print('Compra: "Colchon" - Valor de: 12.000 ars')
+c = total
+d = 12000
+substraction = c - d
+print( 'Saldo:',substraction,'ars')
+
+###################################
+#      /\         /\              #
+#      ||'''''''''||              #
+#      |  o    o  ||       //     #
+#      |    ♥     ||       //     #
+#       \   ww    ||       //     #
+#         \ ____// **********     #
+#          ******************     #
+#         **   **      **  **     #
+#        *      *      **  **     #
+#                                 #
+###################################
+
